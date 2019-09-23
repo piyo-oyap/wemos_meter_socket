8da205f7f2e108763eae3ac8818885bc3c9784c2
oyap <c.salvadora26@gmail.com>
2019-09-23 23:41:10 +0800

    working arduino side

--- SmartSockArduino/SmartSockArduino.ino
+++ SmartSockArduino/SmartSockArduino.ino
@@ -1,51 +1,75 @@
+#include <PZEM004Tv30.h>
 #include <LiquidCrystal_I2C.h>
 #include<EEPROM.h>
 
-bool power = false;
+#define DEBUG
+
+bool relayPower = false, swState = false;
 int power_int = EEPROM.read(0);
-int ctr, input= 7, high_time, low_time, volts, amps, freq, kWh;
-float time_period;
+const int input= 6, sw = 7;
+float time_period = 0, Voltage=0, Current=0, Frequency = 0, Power=0, Energy = 0, PF = 0;
 char str[30];
 
+PZEM004Tv30 pzem(&Serial1);
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
+//  EEPROM.update(0,0);
   Serial.begin(9600);
   Serial3.begin(115200);
   lcd.begin();
   lcd.backlight();
   lcd.clear();
   pinMode(input, INPUT);
+  pinMode(sw, OUTPUT);
+  digitalWrite(sw, HIGH);
 }
 //
 void loop() {
-  high_time = pulseIn(input, HIGH);
-  low_time = pulseIn(input, LOW);
-  time_period = high_time + low_time;
-  time_period /= 273;
-  amps = random(0,100);
-  volts = random(0,250);
-  freq = random(40,70);
-  kWh = (volts*amps)/1000;
+#ifdef DEBUG
+  if(Serial.available()){
+    if(Serial.read()=='a'){
+      toggle();
+      Serial.println("Toggle");
+    }
+  }
+#endif
+  Voltage = pzem.voltage();
+  Current = pzem.current();
+  Energy  = pzem.energy();
+  Power = pzem.power();
+  PF = pzem.pf();
+  Frequency = pzem.frequency();
   lcdPrint();
   echoOutput();
   sendRawData();
   delay(500);
+  if (Voltage == NAN) {
+
+    Serial.print("NO CURRENT WIW");
+  }
 
 }
 
 void lcdPrint(){
+  char valBuffer[15];
+  char valBuffer2[15];
   lcd.setCursor(0,0);
-  sprintf(str, "Volts:%4d Freq:%4d", volts, freq);
+  dtostrf(Voltage, 6, 1, valBuffer);
+  dtostrf(PF, 5, 2, valBuffer2);
+  sprintf(str, "Volt:%s PF:%s", valBuffer, valBuffer2);
   lcd.print(str);
   lcd.setCursor(0,1);
-  sprintf(str, "Amps:%5d kWh:%5d", amps, kWh);
+  dtostrf(Frequency, 5, 1, valBuffer);
+  sprintf(str, "Freq:%s  PI:%5d", valBuffer, power_int);
   lcd.print(str);
   lcd.setCursor(0,2);
-  sprintf(str, "P.I:%6d", power_int);
+  dtostrf(Current, 7, 3, valBuffer);
+  sprintf(str, "Amp:%s", valBuffer);
   lcd.print(str);
   lcd.setCursor(0,3);
-   sprintf(str, " ");
+  dtostrf(Energy, 4, 3, valBuffer);
+  sprintf(str, "kWh:  %s",valBuffer);
   lcd.print(str);
 }
 
@@ -55,8 +79,15 @@ void echoOutput() {
     if (str == "!")
     {
       toggle();
+    #ifdef DEBUG
       Serial.println("toggled");
-    } else {
+    #endif
+    } else if (str.indexOf("192") > 0) {
+      lcd.setCursor(0,0);
+      lcd.print(str);
+      Serial.println( str );
+    }
+    else {
       Serial.println( str );
     }
       
@@ -64,21 +95,22 @@ void echoOutput() {
 }
 
 void toggle() {
-  power = !power;
+  relayPower = !relayPower;
+  digitalWrite(sw, !relayPower);
 }
 
 void sendRawData() {
   Serial3.print('!');
-  Serial3.print(volts);
+  Serial3.print(Voltage);
   Serial3.print(',');
-  Serial3.print(amps);
+  Serial3.print(Current);
   Serial3.print(',');
-  Serial3.print(freq);
+  Serial3.print(Frequency);
   Serial3.print(',');
-  Serial3.print(kWh);
+  Serial3.print(Energy, 3);
   Serial3.print(',');
   Serial3.print(power_int);
   Serial3.print(',');
-  Serial3.print(power);
+  Serial3.print(relayPower);
   Serial3.print('\n');
 }
